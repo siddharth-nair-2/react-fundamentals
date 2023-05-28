3ae435e2f6a9da6363a48d43442b6773e4c884da
siddharth-nair-2 <nairsid2@gmail.com>
2023-05-27 22:00:31 -0400

    test

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -25,7 +25,7 @@ function App() {
   return (
     <div className="keys">
       <button disabled={items.length >= allItems.length} onClick={addItem}>
-        add items
+        add itemssss
       </button>
       <ul>
         {items.map(item => (
